c4c9ab5a73fd285bd1840938541d48e3979a280e
Matthew Kelly <badgerati42@gmail.com>
2020-02-12 20:38:20 +0000

    #49: Adds support for scrolling to an element

--- src/Public/Page.ps1
+++ src/Public/Page.ps1
@@ -9,20 +9,36 @@ function Move-MonoclePage
 
         [Parameter(ParameterSetName='Position')]
         [int]
-        $Position
+        $Position,
+
+        [Parameter(ParameterSetName='Element', ValueFromPipeline=$true)]
+        [OpenQA.Selenium.IWebElement]
+        $Element
     )
 
-    if ($PSCmdlet.ParameterSetName -ieq 'to') {
-        $size = Get-MonoclePageSize
-        $Position = (@{
-            Bottom = $size.Height
-            Middle = $size.Height * 0.5
-            Top = 0
-        })[$To]
-    }
+    switch ($PSCmdlet.ParameterSetName.ToLowerInvariant()) {
+        # literal positions
+        { @('to', 'position') -icontains $_ } {
+            if ($PSCmdlet.ParameterSetName -ieq 'to') {
+                $size = Get-MonoclePageSize
+                $Position = (@{
+                    Bottom = $size.Height
+                    Middle = $size.Height * 0.5
+                    Top = 0
+                })[$To]
+            }
+    
+            Write-MonocleHost -Message "Scrolling to: $Position"
+            Invoke-MonocleJavaScript -Arguments $Position -Script 'window.scrollTo(0, arguments[0])' | Out-Null
+        }
 
-    Write-MonocleHost -Message "Scrolling to: $Position"
-    Invoke-MonocleJavaScript -Arguments $Position -Script 'window.scrollTo(0, arguments[0])' | Out-Null
+        # elements
+        'element' {
+            $id = Get-MonocleElementId -Element $Element
+            Write-MonocleHost -Message "Moving page to element: $($id)"
+            Invoke-MonocleJavaScript -Arguments $Element -Script 'arguments[0].scrollIntoView(true)' | Out-Null
+        }
+    }
 }
 
 function Get-MonoclePageSize
